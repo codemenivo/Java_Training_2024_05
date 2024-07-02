578352e67f1c2c2bb82fdcc559e71c3b047d40d2
codeman <codemen@abv.bg>
2024-07-02 19:12:56 +0300

    Initial commit for Generics Exercise

--- src/Sirma_May_2024_17_Generics/Ex_17_03_GenericScale/Main.java
+++ src/Sirma_May_2024_17_Generics/Ex_17_03_GenericScale/Main.java
@@ -3,7 +3,7 @@ package Sirma_May_2024_17_Generics.Ex_17_03_GenericScale;
 public class Main {
     public static void main(String[] args) {
 
-        Scale<Integer> intScale = new Scale<>(15, 18);
+        Scale<Integer> intScale = new Scale<>(15, 20);
         System.out.println(intScale.getHeavier());
 
         Scale<String> strScale = new Scale<>("Gosho", "Gosho");
